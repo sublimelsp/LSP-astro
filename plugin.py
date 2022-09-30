deabc91ae4754f5e5b81d2ea22460c4b40fbb14a
deathaxe <deathaxe82@googlemail.com>
2022-09-30 21:59:46 +0200

    Add Typescript language support (#13)
    This commit ...
    
    1. adds typescript package dependency
    2. adds default configuration templates for `typescript.serverPath`
    3. adds logic to find and configure project specific typescript package
    
    By default LSP-astro prefers a typescript language server of the Astro
    project being worked at. If it can't be found, the server bundled with
    LSP-astro is used.

--- plugin.py
+++ plugin.py
@@ -1,6 +1,6 @@
-from LSP.plugin.core.typing import Tuple
-from lsp_utils import NpmClientHandler
 import os
+
+from lsp_utils import NpmClientHandler
 
 
 def plugin_loaded():
@@ -23,6 +23,66 @@ class LspAstroPlugin(NpmClientHandler):
         "nodeServer.js",
     )
 
+    typescript_relpath = os.path.join(
+        'node_modules',
+        'typescript',
+        'lib',
+        'tsserverlibrary.js'
+    )
+
     @classmethod
-    def minimum_node_version(cls) -> Tuple[int, int, int]:
+    def find_typescript_path(cls, path):
+        """
+        Find typescript in the package specified ``path`` belongs to.
+
+        :param path:
+            The absolute path of a directory to use as starting point
+
+        :returns:
+            The absolute path to typescript language-server if exists.
+        """
+        name = True  # to get started with loop
+        while path and name:
+            if os.path.isfile(os.path.join(path, 'package.json')):
+                typescript_path = os.path.join(path, cls.typescript_relpath)
+                if os.path.isfile(typescript_path):
+                    return typescript_path
+                break
+
+            path, name = os.path.split(path)
+
+        return None
+
+    @classmethod
+    def is_allowed_to_start(cls, window, initiating_view, workspace_folders, configuration):
+        if configuration is None:
+            return
+
+        if configuration.init_options.get('typescript.serverPath'):
+            return  # don't find the `typescript.serverPath` if it was set explicitly in LSP-astro.sublime-settings
+
+        typescript_path = None
+
+        # try to find package root and typescript based on current file to support
+        # nested packages, which are not directly added as folder to sidebar
+        if initiating_view:
+            file_name = initiating_view.file_name()
+            if file_name:
+                typescript_path = cls.find_typescript_path(os.path.dirname(file_name))
+
+        # try to find typescript from first workspace folder
+        if workspace_folders and not typescript_path:
+            typescript_path = cls.find_typescript_path(workspace_folders[0].path)
+
+        # use typescript bundled with LSP-astro
+        if not typescript_path:
+            typescript_path = os.path.join(cls._server_directory_path(), cls.typescript_relpath)
+
+        if not typescript_path:
+            return
+
+        configuration.init_options.set('typescript.serverPath', typescript_path)
+
+    @classmethod
+    def minimum_node_version(cls):
         return (14, 0, 0)
